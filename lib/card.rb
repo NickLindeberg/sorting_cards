afc928de3fd2e00c054d2a558a85f2e852574ce0
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-03 08:10:45 -0600

    adding methods to guess

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,9 @@
 class Card
   attr_reader :value, :suit
   def initialize (value_number, suit_guess)
-    @value = value_number
     @suit = suit_guess
+    @value = value_number
+    
   end
 
 
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -2,10 +2,42 @@ require "../lib/guess.rb"
 require "../lib/card.rb"
 require "minitest/autorun"
 require 'minitest/pride'
+require 'pry'
 
 class GuessTest <Minitest::Test
   def test_if_card_exists
-  card = Card.new("10", "Hearts")
-  assert_instance_of Card, card
+    card = Card.new("10", "Hearts")
+    assert_instance_of Card, card
   end
+
+  def test_if_card_and_guess_talk
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+    assert_instance_of Card, guess.card
+  end
+
+  def test_if_response_is_good
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10", "Hearts")
+    expected = ("10 of Hearts")
+binding.pry
+    assert_equal expected, guess.response
+
+  end
+
+  def test_if_guess_is_t_or_f
+    guess = Guess.new("10", "Hearts")
+    assert_equal true, guess.correct?
+
+  end
+
+  def test_if_feedback_shows_correct
+    guess = Guess.new("10", "Hearts")
+    expected =
+    assert_equal "Correct", guess
+
+  end
+
+
+
 end
