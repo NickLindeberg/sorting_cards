0aea5e40239872f257aa96fed66b4f49b4cdffb6
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-03 17:53:27 -0600

    complete rework

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,6 @@
 class Card
-  attr_reader :value, :suit
+  attr_reader :value,
+              :suit
   def initialize (value_number, suit_guess)
     @suit = suit_guess
     @value = value_number
@@ -9,4 +10,3 @@ class Card
 
 
 end
-######
