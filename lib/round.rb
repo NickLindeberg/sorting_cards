76c459f7e7a37ecefb1293250bdc71c8d84d1258
Nick Lindeberg <lindebergnick@gmail.com>
2018-07-04 14:50:17 -0600

    finished it 2

--- lib/round.rb
+++ lib/round.rb
@@ -8,10 +8,15 @@ class Round
               :deck,
               :current_card,
               :record_guess,
-              :hash
+              :hash,
+              :number_correct
+              :percent_correct
   def initialize(deck)
     @deck = deck
     @guesses = []
+    @number_correct = 0
+    @total_guess = 0
+
 
   end
 
@@ -25,13 +30,19 @@ class Round
     response = "#{value} of #{suit}"
     new_guess = Guess.new(response, current_card)
     @guesses << new_guess
+    if new_guess.correct? == true
+      @number_correct += 1
+      @total_guess += 1
+    else
+      @total_guess += 1
+    end
+
+
+
+    def percent_correct
+      @number_correct.to_f / @total_guess.to_f * 100
+
+    end
     return new_guess
   end
-
-  def number_correct
-  end
-
-  
-
-
 end
